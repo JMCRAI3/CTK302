a26265bd4282ee770dcea570ce1323dbab4ed177
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-09-06 18:30:43 -0500

    Assignment2

--- Unit1/Fonts/sketch.js
+++ Unit1/Fonts/sketch.js
@@ -1,7 +1,17 @@
+let f1, f2 ;
+
 function setup() {
   createCanvas(500, 500);
+  f1 = loadFont("Assets/BackToSchool.ttf");
+  f2 = loadFont("Assets/Smiley.ttf");
+  textAlign(CENTER);
 }
 
 function draw() {
-
+  background("black");
+  fill("white");
+textFont(f1, 48);
+text("hello there!", width/2, 100);
+textFont(f2, 24);
+text("I am Obi Wan", width/2, 200);
 }
