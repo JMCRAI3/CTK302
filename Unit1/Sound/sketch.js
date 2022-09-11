a03a2906c9c8b5250c731d19a94f55f0f37fd771
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-09-11 13:41:08 -0500

    addons

--- Unit1/Sound/sketch.js
+++ Unit1/Sound/sketch.js
@@ -1,5 +1,12 @@
+let funday ;
+
+function preload() {
+  funday = loadSound("Assets/funday.,p3");
+}
+
 function setup() {
   createCanvas(500, 500);
+  funday = loadSound("Assets/funday.mp3");
 }
 
 function draw() {
