44cfce8775d63849f6fda013af6fe61ab59d62dd
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-09-11 14:17:39 -0500

    new

--- Unit1/Sound/sketch.js
+++ Unit1/Sound/sketch.js
@@ -13,9 +13,18 @@ function setup() {
 function draw() {
   background("black");
   fill("white");
+  text("The name is Funday and I like it because it has a really good beat", 100, 100, 400, 400);
+
 
 }
 
+function mouseReleased(){
+  if (funday.isPlaying()) {
+    funday,pause() ;
+  } else {
+    funday.play() ;
+  }
+}
 
 function touchStarted() {
   getAudioContext().resume();
