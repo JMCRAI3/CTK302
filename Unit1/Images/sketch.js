b0adabefa5afccf0cdaa9a86511b31cf3563dc6e
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-09-13 10:51:01 -0500

    update

--- Unit1/Images/sketch.js
+++ Unit1/Images/sketch.js
@@ -4,9 +4,9 @@ let i1, i2, i3 ;
 
 function setup() {
   createCanvas(500, 500);
-  i1 = loadImage("chococake.png");
-  i2 = loadImage("horsebackriding.png");
-  i3 = loadImage("painting.jpg");
+  i1 = loadImage("Assets/chococake.png");
+  i2 = loadImage("Assets/horsebackriding.png");
+  i3 = loadImage("Assets/painting.jpg");
   imageMode(CENTER);
 }
 
