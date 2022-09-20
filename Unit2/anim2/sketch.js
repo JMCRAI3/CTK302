6b74081961bae54f290ae5c3f10398ec4c08cdd8
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-09-20 10:35:20 -0500

    new avatar

--- Unit2/anim2/sketch.js
+++ Unit2/anim2/sketch.js
@@ -1,7 +1,27 @@
+let x = 0;
+
+
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
+  background (100);
+translate(x, 0) ;
+  avatar() ;
+  x += 5; 
+  if (x > width) {
+    x = -300;
+  }
+}
+
+
+function avatar() {
+
+  fill("red");
+  ellipse(387, 187, 100, 100);
+  ellipse(391, 245, 100, 100);
 
 }
+
+
