230b687f403e3af432015ae9a00a1716d48a27f6
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-09-20 10:02:41 -0500

    new

--- Unit2/anim1/sketch.js
+++ Unit2/anim1/sketch.js
@@ -1,7 +1,15 @@
+
+let x = 0;
+
 function setup() {
   createCanvas(500, 500);
 }
 
 function draw() {
-
+  background (100);
+  rect(x, 100, 100, 100);
+  x = x+1;
+  if (x>width) {
+    x=0;
+  }
 }
