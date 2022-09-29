7a546223bce9b5f308556ef97fea4e7603d6add2
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-09-29 15:39:56 -0500

    jokes

---
+++ Unit2/Jokes/joke1-1/sketch.js
@@ -0,0 +1,26 @@
+
+let state = 0 ;
+
+function setup() {
+  createCanvas(500, 500);
+  rectMode(CENTER) ;
+}
+
+function draw() {
+
+switch(state) {
+  case 0 :
+    background('red') ;
+    Text("why did the chx", width/2, height/2, 400, 400) ;
+
+    break ;
+
+
+    case 1:
+      break ;
+
+
+}
+
+
+}
