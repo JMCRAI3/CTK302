08eeb679419f2adef86ab0757613c12dadae08fe
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-10-04 10:34:38 -0500

    stop

--- Unit2/Stoplight/sketch.js
+++ Unit2/Stoplight/sketch.js
@@ -33,7 +33,7 @@ function draw() {
     ellipse(width/2, height/2, 150, 150);
     fill("grey");
     ellipse(width/2, height/2 + 170, 150, 150);
-    v = 4;
+    v = 5;
       break;
 
     case 2: //red light
@@ -42,7 +42,9 @@ function draw() {
     fill("grey");
     ellipse(width/2, height/2, 150, 150);
     ellipse(width/2, height/2 + 170, 150, 150);
-    v = 0;
+    if (x >= width/ 2 && x <= width/2 + v) {
+      v = 0;
+    }
       break;
 
   }
