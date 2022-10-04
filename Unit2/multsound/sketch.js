a095bbf36e73e0ebdd79f70574d0ac79039a273b
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-10-04 00:32:56 -0500

    final

--- Unit2/multsound/sketch.js
+++ Unit2/multsound/sketch.js
@@ -1,4 +1,13 @@
-let state = 0;
+let state = -1;
+let s1, s2, s3 ;
+
+
+function preload() {
+  s1 = loadSound("Assets/creativeminds.mp3") ;
+  s2 = loadSound("Assets/evolution.mp3") ;
+  s3 = loadSound("Assets/ukulele.mp3") ;
+}
+
 
 function setup() {
   createCanvas(800, 800);
@@ -10,23 +19,47 @@ function draw() {
   
   switch (state) {
 
+    case -1:
+      text("Please Click To Start", 100, 100);
+      break;
+
     case 0:
-      text("0", 100, 100);
+      background(0,0,100);
+      text("Creative Minds", 100, 100);
+      if (!s1.isPlaying()){
+        s1.play() ;
+      }
       break;
 
     case 1:
-      text("1", 100, 100);
+      background(100,0,0);
+      text("Evolution", 100, 100);
+      if (!s2.isPlaying()){
+        s2.play() ;
+      }
       break;
 
     case 2:
-      text("2", 100, 100);
+      background(50,0,50);
+      text("Ukulele", 100, 100);
+      if (!s3.isPlaying()){
+        s3.play() ;
+      }
       break;
 
   }
 }
 
 function mouseReleased() {
+  s1.stop() ;
+  s2.stop() ;
+  s3.stop() ;
   state++;
   if (state > 2) state = 0;
 
 }
+
+function touchStarted() {
+  getAudioContext().resume();
+}
+
