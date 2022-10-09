a8d2f210a493b59ff68672beb5e3ad229b655229
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-10-08 23:25:30 -0500

    new

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,7 +1,11 @@
 let numberOfTouches ;
+let i1, i2, i3;
 
 function setup() {
   createCanvas(400, 400);
+  i1 = loadImage("Assets/beagle.jpg");
+  i2 = loadImage("Assets/jackrussel.jpg");
+  i3 = loadImage("Assets/pug.jpg");
 }
 
 function draw() {
@@ -11,21 +15,24 @@ function draw() {
   
   switch(numberOfTouches) {
     case 0: 
-      text("no one is touching the screen", 5, 22) ; 
+      text("No Licks", 5, 22) ; 
       break ;
       
       case 1: 
-       text("it's kind of lonely here", 5, 22) ; 
+       text("One Lick", 5, 22) ; 
+       image(i1,width/2, height/2, 100, 100);
       // put a picture here
       break ;
       
       case 2:
-      text("two fingers are touching the screen", 5, 22) ; 
+      text("Two Licks", 5, 22) ; 
+      image(i2,width/2, height/2, 100, 100);
             // put a picture here
       break ;
       
       case 3:
-     text("three are touching the screen", 5, 22) ; 
+     text("Three Licks", 5, 22) ; 
+     image(i3,width/2, height/2, 100, 100);
             // put a picture here
       break ;
     
