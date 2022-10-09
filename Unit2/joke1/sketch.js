0ebbdb26a88299c15d18e45678b9858d91a152c0
JMCRAI3 <JMCRAI3@ilstu.edu>
2022-10-08 22:38:16 -0500

    update

--- Unit2/joke1/sketch.js
+++ Unit2/joke1/sketch.js
@@ -9,12 +9,12 @@ function draw() {
   switch (state) {
    case 0 :
       background("red") ;
-      text("why did \nthe chicken", width / 2, height / 2) ;
+      text("Why does Princess Leia keep her hair tied up in buns?", width / 2, height / 2) ;
        break ;
 
     case 1:
       background("blue") ;
-      text("because", width / 2, height / 2) ;
+      text("So it doesn't Hang Solow.", width / 2, height / 2) ;
       break ;
   }
 }
